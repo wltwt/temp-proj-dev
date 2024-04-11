1c3b382e38ce1888402e28ba8cc2374425b69d5c
wltwt <wltwt@proton.me>
2024-04-11 16:41:11 +0200

    test-setup

--- iels-2001-prosjekt-esp32/src/main.cpp
+++ iels-2001-prosjekt-esp32/src/main.cpp
@@ -1,28 +1,135 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+#include <Wire.h>
 
 #define SDA 16
 #define SCL 17
 
+void setup_wifi();
+void reconnect();
+void callback(char* topic, byte* message, unsigned int length);
+
+// WiFi
+const char *ssid = "NTNU-IOT"; // Enter your WiFi name
+const char *password = "";  // Enter WiFi password
+
+// Add your MQTT Broker IP address, example:
+//const char* mqtt_server = "192.168.1.144";
+const char* mqtt_server = "10.25.18.209";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
 
 
 void setup() {
+  Serial.begin(115200);
 
   Wire.begin();
+
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
   
 }
 
 void loop() {
   static int x = 0;
 
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
-  Wire.beginTransmission(0x01);
-  Wire.write("x is");
-  Wire.write(x);
-  Wire.endTransmission();
-  x++;
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
 
-  delay(500);
+    client.publish("cars/quality", "testing");
+
+    Wire.beginTransmission(0x01);
+    Wire.write("x is");
+    Wire.write(x);
+    Wire.endTransmission();
+    x++;
 
 
+  }
+
+
+
+}
+
+
+
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+    }
+  }
 }
